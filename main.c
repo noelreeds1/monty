f080dc601c9dccdce619543c3e586431194bbdd2
Noel Reeds <noelreeds01.com>
2023-12-15 14:34:54 +0000

    handles the basic entry logic into the interpreter

---
+++ main.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+/**
+ * main - Entry point of a C program
+ *
+ *@argc - refers to the number of args passed.
+ *
+ *@argv - refers to the arguments passed.
+ *
+ *Return: Always 0.
+ */
+int main(int argc, char **argv)
+{
+    FILE *stream;
+    char *lineptr;
+    size_t buff;
+    ssize_t num_read;
+
+    if (argc != 2)
+    {
+        fprintf(stderr, "USAGE: monty file\n");
+        exit(EXIT_FAILURE);
+    }
+    stream = fopen(argv[1], "r");
+    if (stream == NULL)
+    {
+        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+        exit(EXIT_FAILURE);
+    }
+    while((num_read = getline(&lineptr, &buff, stream)) != -1)
+    {
+
+        char *token = strtok(lineptr, " ");
+        char *token2 = strtok(NULL, " ");
+    }
+    return (0);
+}
